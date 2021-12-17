477638f6738bae307ea81d2cdf072477272dfaf3
Kartik Patel <kartik.patel@multidots.com>
2021-12-17 22:26:06 +0530

    Update process.php

--- process.php
+++ process.php
@@ -50,14 +50,15 @@ if( $_FILES['files']['name'] ) {
                 $zip->close();
                 unlink( $myFile );
 
-                $wpstandard = filter_input( INPUT_POST, 'wpstandard', FILTER_SANITIZE_STRING );
-                $wpseverity = filter_input( INPUT_POST, 'wpseverity', FILTER_SANITIZE_STRING );
-                $wpreporterr= filter_input( INPUT_POST, 'wpreporterr', FILTER_SANITIZE_STRING );
+                $wpstandard     = filter_input( INPUT_POST, 'wpstandard', FILTER_SANITIZE_STRING );
+                $wpseverity     = filter_input( INPUT_POST, 'wpseverity', FILTER_SANITIZE_STRING );
+                $wpreporterr    = filter_input( INPUT_POST, 'wpreporterr', FILTER_SANITIZE_STRING );
 
-                $wpstandard = ! empty( $wpstandard ) ? $wpstandard : 'wpvipgo';
-                $wpseverity = ! empty( $wpseverity ) ? $wpseverity : 'severity6andplus';
-                $wpstandard = ! empty( $wpstandard ) ? $wpstandard : 'wperr';
+                $wpstandard     = ! empty( $wpstandard ) ? $wpstandard : 'wpvipgo';
+                $wpseverity     = ! empty( $wpseverity ) ? $wpseverity : 'severity6andplus';
+                $wpreporterr    = ! empty( $wpreporterr ) ? $wpreporterr : 'wperr';
 
+                // Set standard
                 switch( $wpstandard ) {
                     case 'wpvipgo':
                         $standard = 'WordPress-VIP-Go';
@@ -72,12 +73,43 @@ if( $_FILES['files']['name'] ) {
                         $standard = 'WordPress-VIP-Go';
                 }
 
-                // phpcs --standard=WordPress-VIP-Go -sp --basepath=. --ignore=vendor --warning-severity=6 --error-severity=6 --report=csv
+                // Set severity level
+                switch( $wpseverity ) {
+                    case 'severity6andplus':
+                        $level = 6;
+                        break;
+                    case 'severity5':
+                        $level = 5;
+                        break;
+                    case 'severity4andunder':
+                        $level = 4;
+                        break;
+                    case 'severityall':
+                        $level = '';
+                        break;
+                    default:
+                        $level = 6;
+                }
 
-                $command_string = "vendor/bin/phpcs --standard=" . $standard . " " . $destination . " --report=csv --report-file=" . $destination . "/" . $filenoext . ".csv";
+                // Set errors and warnings
+                $warning_error = '';
+                if( ! empty( $level ) ) {
+                    if( 'wperr' == $wpreporterr ) {
+                        $warning_error .= ' --error-severity=' . $level;
+                    } else if( 'wperr' == $wpreporterr ) {
+                        $warning_error .= ' --warning-severity=' . $level;
+                    } else if( 'wperrandwar' == $wpreporterr ) {
+                        $warning_error .= ' --error-severity=' . $level;
+                        $warning_error .= ' --warning-severity=' . $level;
+                    } else {
+                        $warning_error .= ' --error-severity=' . $level;
+                    }
+                }
+
+                // Create command string
+                $command_string = "vendor/bin/phpcs --standard=" . $standard . " " . $destination . $warning_error . " --report=csv --report-file=" . $destination . "/" . $filenoext . ".csv";
                 
                 $output = shell_exec( $command_string );
-                // echo '<pre>-----PHPCS OUTPUT-----';
                 var_dump( $output );
                 exit( $command_string );
                 // echo '</pre>';
@@ -91,4 +123,4 @@ if( $_FILES['files']['name'] ) {
         }
     }
 }
-die();
+exit();
