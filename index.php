a71f7b4aaaba48154db529b21d5301b18f5cf9e4
Kartik Patel <kartik.patel@multidots.com>
2021-12-17 18:19:33 +0530

    Troubleshooting for PHPCS command execute or not

--- index.php
+++ index.php
@@ -48,7 +48,10 @@ if( $_FILES['files']['name'] ) {
 
                 $command_string = "phpcs --standard=WordPressVIPMinimum " . $destination; // . " --report=csv --report-file=" . $filename . ".csv";
                 echo $command_string = "phpcs -i";
-                shell_exec( 'export PATH="$HOME/vendor/bin:$PATH"' );
+                $output = shell_exec( 'export PATH="$HOME/vendor/bin:$PATH"' );
+                echo '<pre>-----OUTPUT-----';
+                print_r( $output );
+                echo '</pre>';
                 $output = shell_exec( $command_string );
                 
                 // $output = null;
